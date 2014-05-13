73eb5a21f95a65540b7ac13751c7e9ec41d69816
Brett Slabaugh <championoftyria@gmail.com>
2014-05-13 09:15:31 -0400

    Added simple clipping
    Will need to change when more rooms are implemented

--- infinidungeon/Camera.cpp
+++ infinidungeon/Camera.cpp
@@ -87,6 +87,9 @@ void Camera::move( glm::vec3 directions, glm::vec2 rotations, float frametime ) 
     _position += directions[1] * camera_pitch_direction * frametime * _speed;
     // up and down flying - only y-axis could be affected
     _position.y += directions[2] * frametime * _speed;
+	// restrict camera to room dimensions
+	_position.x = glm::clamp(_position.x, -22.0f, 22.0f);
+	_position.z = glm::clamp(_position.z, -21.0f, 21.0f);
 
     m_mView = glm::lookAt(_position, _position + camera_roll_direction,
                         glm::cross(camera_roll_direction, camera_pitch_direction));
