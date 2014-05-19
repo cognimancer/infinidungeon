94a0199c44308e273c8039b9edf5858d552de51b
Brett Slabaugh <championoftyria@gmail.com>
2014-05-19 14:49:19 -0400

    Modularized room construction
    Rooms now know which directions are exits

--- infinidungeon/Room.cpp
+++ infinidungeon/Room.cpp
@@ -1,8 +1,56 @@
 #include "Room.h"
 
-Room::Room( std::string name, int row, int col, float rotation ) : _name( name ), _rotation( rotation ), _row( row ), _col( col ),
-		north( nullptr ), east( nullptr ), south( nullptr ), west( nullptr ) {
-	ModelManagerClass::GetInstance()->LoadModel( "Room2.obj", name );
+Room::Room( std::string name, int row, int col, int roomType, int rotation ) : _name( name ), _rotation( rotation ), _row( row ), _col( col ),
+		north( nullptr ), east( nullptr ), south( nullptr ), west( nullptr ),
+		exitNorth(false), exitEast(false), exitSouth(false), exitWest(false){
+	//load model for given roomtype
+	std::string modelString = "Room" + std::to_string(roomType) + ".obj";
+	ModelManagerClass::GetInstance()->LoadModel( modelString, name );
+	
+	//set initial available exits for this roomtype
+	//these are temporary; true exits will be modified by rotation
+	boolean n = false;
+	boolean s = false;
+	boolean e = false;
+	boolean w = false;
+	switch(roomType) {
+	case ROOM_DEADEND: n = true; break;
+	case ROOM_ELBOW: n = true; w = true; break;
+	case ROOM_HALL: n = true; s = true; break;
+	case ROOM_T_JOINT: n = true; w = true; s = true; break;
+	case ROOM_CROSSROADS: n = true; w = true; s = true; e = true; break;
+	default:
+		std::cout << "Error: illegal room type " << roomType << " in " << name << std::endl;
+		system("pause");
+		exit(0);
+	}
+	//modify available exits due to room rotation
+	switch(rotation) {
+	case 0: break;
+	case 90: 
+		exitNorth = w;
+		exitEast = n;
+		exitSouth = e;
+		exitWest = s;
+		break;
+	case 180: 
+		exitNorth = s;
+		exitEast = w;
+		exitSouth = n;
+		exitWest = e;
+		break;
+	case 270:
+		exitNorth = e;
+		exitEast = s;
+		exitSouth = w;
+		exitWest = n;
+		break;
+	default:
+		std::cout << "Error: illegal rotation value " << rotation << " in " << name << std::endl;
+		system("pause");
+		exit(0);
+	}
+
 	setModelMatrix();
 }
 
